f8f44a5c7a205877679042adb2552e9c71858cbc
Shubham Sharma <shubham@iastate.edu>
2019-11-02 00:38:55 -0500

    Working with DB

---
+++ app/src/main/java/com/teamwd/chipin/Activities/ActivityDatabaseTest.java
@@ -0,0 +1,26 @@
+package com.teamwd.chipin.Activities;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+import com.teamwd.chipin.R;
+
+public class ActivityDatabaseTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_database_test);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+}
--- app/src/main/java/com/teamwd/chipin/Activities/ActivityMain.java
+++ app/src/main/java/com/teamwd/chipin/Activities/ActivityMain.java
@@ -2,7 +2,9 @@ package com.teamwd.chipin.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.crashlytics.android.Crashlytics;
 import com.teamwd.chipin.R;
@@ -13,5 +15,10 @@ public class ActivityMain extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openActivity(View view) {
+        Intent myIntent = new Intent(this, ActivityDatabaseTest.class);
+        this.startActivity(myIntent);
     }
 }
