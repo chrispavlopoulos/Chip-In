f56a5be3ce7e9a64343af0d7fba2720d14c1a272
Shubham Sharma <shubham@iastate.edu>
2019-11-02 00:22:53 -0500

    Added firebase

--- app/src/main/java/com/teamwd/chipin/Activities/ActivityMain.java
+++ app/src/main/java/com/teamwd/chipin/Activities/ActivityMain.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.crashlytics.android.Crashlytics;
 import com.teamwd.chipin.R;
 
 public class ActivityMain extends AppCompatActivity {
