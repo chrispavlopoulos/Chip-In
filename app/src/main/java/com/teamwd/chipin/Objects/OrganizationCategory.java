d24dedc312472161b318b44c9195de483224b0ad
Jacob Laing <wdjmlaing@iastate.edu>
2019-11-02 04:20:43 -0500

    More merge fixes

--- app/src/main/java/com/teamwd/chipin/Objects/OrganizationCategory.java
+++ app/src/main/java/com/teamwd/chipin/Objects/OrganizationCategory.java
@@ -7,14 +7,9 @@ public class OrganizationCategory extends RealmObject {
     private String categoryId;
     private String categoryName;
     private String image;
-    private String charityNavigatorUrl;
-
 
     public OrganizationCategory() {
 
     }
-<<<<<<< HEAD
 
-=======
->>>>>>> ed679443146b1c1d2f376fafca7c96b699ebf397
 }
