6b36f9ec22428146482059aff03f19fbb90a62b7
Chris <pavlopouloschris2@yahoo.com>
2019-11-01 23:32:04 -0500

    Initial commit

---
+++ app/src/main/java/com/teamwd/chipin/Activities/ActivityLogIn.java
@@ -0,0 +1,16 @@
+package com.teamwd.chipin.Activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.teamwd.chipin.R;
+
+public class ActivityLogIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
