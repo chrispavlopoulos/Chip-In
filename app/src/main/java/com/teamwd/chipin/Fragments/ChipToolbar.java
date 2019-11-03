65d7ef8f92ba2f429b8f2923d4396e3740bd9039
Chris <pavlopouloschris2@yahoo.com>
2019-11-03 05:51:38 -0600

    FIXED NAVIGATION BAR BUG, and added search menu icons

---
+++ app/src/main/java/com/teamwd/chipin/Fragments/ChipToolbar.java
@@ -0,0 +1,21 @@
+package com.teamwd.chipin.Fragments;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
+
+public class ChipToolbar extends Toolbar {
+    public ChipToolbar(Context context) {
+        super(context);
+    }
+
+    public ChipToolbar(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ChipToolbar(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
